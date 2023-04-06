edc0ba66fb9c791b28d43207146203e804d384e2
YuliaKhyzhuk <anastezy1@gmail.com>
2023-04-06 04:07:34 +0300

    App refactored

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,33 +1,44 @@
 import React from 'react';
 import { GlobalStyle } from './GlobalStyle';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchContacts } from 'redux/operations';
+import { selectError, selectIsLoading } from 'redux/selectors';
 
 import Section from './Section/Section';
 import Form from './Form/Form';
 import Filter from './Filter/Filter';
 import ContactsList from './ContactsList/ContactsList';
 
+export function App() {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
 
-export function App () {
-     return (
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          fontSize: 16,
-          color: '#010101',
-        }}
-      >
-        <Section title="Phonebook">
-          <Form />
-        </Section>
-        <Section title="Contacts">
-          <Filter />
-          <ContactsList />
-        </Section>
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
 
-        <GlobalStyle />
-      </div>
-    );
-  }
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        fontSize: 16,
+        color: '#010101',
+      }}
+    >
+      <Section title="Phonebook">
+        <Form />
+      </Section>
+      <Section title="Contacts">
+        <Filter />
+        <ContactsList />
+      </Section>
 
-  export default App;
+      <GlobalStyle />
+    </div>
+  );
+}
+
+export default App;
